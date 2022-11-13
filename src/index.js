f089d8123e0eae81f90dab64fc77069c090ba207
CrypticAgro <jtech3029@gmail.com>
2022-11-12 18:00:44 -0700

    completed project

--- src/index.js
+++ src/index.js
@@ -1,3 +1,42 @@
 import { getWeather } from "./getWeather.js";
 
-getWeather("riverton")
+const changeWeatherDisplay = (weather) => {
+  let windMeasurement;
+  if (currentDegrees == "imperial") {
+    windMeasurement = "mph";
+  } 
+  else if (currentDegrees == "metric") {
+    windMeasurement = "kph";
+  }
+  const location = document.getElementById("weather-info-location");
+  location.innerText = `${weather.name}, ${weather.country}`;
+
+  const temperature = document.getElementById("weather-info-temperature");
+  temperature.innerText = Math.round(weather.temperature) + "°F";
+
+  const sky = document.getElementById("weather-info-sky");
+  sky.innerText = weather.weather;
+
+  const wind = document.getElementById("weather-info-wind");
+  wind.innerText = `${weather.wind} ${windMeasurement}`;
+};
+
+const weatherSubmitted = () => {
+  getWeather(input.value, currentDegrees)
+    .then(changeWeatherDisplay)
+    .catch((x) => alert(x));
+  input.value = "";
+};
+
+let currentDegrees = "imperial";
+getWeather("riverton", currentDegrees)
+  .then(changeWeatherDisplay)
+  .catch((x) => alert(x));
+
+const input = document.getElementById("input-field");
+
+input.addEventListener("keypress", (e) => {
+  if (e.key === "Enter") {
+    weatherSubmitted();
+  }
+});
